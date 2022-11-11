81a03be1c4a2f23767c0f57070723d488569d426
Yaniss Guendouzi <guendouziyaniss@gmail.com>
2022-11-11 17:04:44 +0100

    added dark mode

--- .storybook/preview.js
+++ .storybook/preview.js
@@ -1,6 +1,18 @@
 import '../tailwind.css';
+import { themes } from '@storybook/theming';
+
 // https://storybook.js.org/docs/react/writing-stories/parameters#global-parameters
 export const parameters = {
   // https://storybook.js.org/docs/react/essentials/actions#automatically-matching-args
   actions: { argTypesRegex: '^on.*' },
+  darkMode: {
+    // Override the default dark theme
+    dark: { ...themes.dark },
+    // Override the default light theme
+    light: { ...themes.normal, appBg: 'white' },
+    darkClass: 'dark',
+    lightClass: 'light',
+    classTarget: 'html',
+    stylePreview: true,
+  },
 };
